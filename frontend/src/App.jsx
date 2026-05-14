83622eaeff490fc670bb4cc6a108fc827d150221
Haseeb <haseebmine24@gmail.com>
2026-05-14 07:57:36 +0500

    first commit//..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -132,10 +132,9 @@ function App() {
 
     try {
       const response = await axios.post(
-        'https://8000-itbs3e41rgze8pg75vw0h-c232b2cc.us2.manus.computer/api/generate-tryon',
+        'https://8000-ia5q4354dcksbxzcdl97w-7332bf72.us2.manus.computer/api/generate-tryon',
         formData
       );
-      
 
       console.log("API RESPONSE:", response.data);
 
